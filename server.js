8dfea5713e61f7a53e6b19dade3a1b5f7f74aeb6
박성수 <49583963+YEONDUJIPSA@users.noreply.github.com>
2025-11-25 12:40:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require('cors');
 
 const NUM_LINES = 10;
 const NUM_STEPS = 12;
 
-app.use(cors());
 app.use(express.json());
 // 'public' 폴더를 정적 파일 경로로 설정
 app.use(express.static(path.join(__dirname, 'public')));
